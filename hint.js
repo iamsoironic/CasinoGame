d0603efbddffe300708d51e27b25c24b9ef501f9
iamsoironic <officialsoironic@gmail.com>
2020-02-12 23:44:13 -0800

    Final commit

--- hint.js
+++ hint.js
@@ -1,85 +1,204 @@
-// WALLET TOTAL
-let currentWallet = 0
+let deck1 = {"2 Hearts": 2,
+            "3 Hearts": 3,
+            "4 Hearts": 4,
+            "5 Hearts": 5,
+            "6 Hearts": 6,
+            "7 Hearts": 7,
+            "8 Hearts": 8,
+            "9 Hearts": 9,
+            "10 Hearts": 10,
+            "J Hearts": 11,
+            "Q Hearts": 12,
+            "K Hearts": 13,
+            "A Hearts": 14,
 
-// PLAYER NAME PROMPT
-let player = prompt("Welcome to the House of Broken Dreams Casino! What is your name?:-");
+            "2 Diamonds": 2,
+            "3 Diamonds": 3,
+            "4 Diamonds": 4,
+            "5 Diamonds": 5,
+            "6 Diamonds": 6,
+            "7 Diamonds": 7,
+            "8 Diamonds": 8,
+            "9 Diamonds": 9,
+            "10 Diamonds": 10,
+            "J Diamonds": 11,
+            "Q Diamonds": 12,
+            "K Diamonds": 13,
+            "A Diamonds": 14,
 
-// WALLET BET PROMPT
-currentWallet = prompt(`\nHello ${player}, you seem like a trustworthy gambler!\n\nPlease enter an amount you would like to play with today! Keep in mind that each guess costs $10. -$`);
-alert(`\n${player} has decided to play with a total of $${currentWallet} today, let's get ready for our first game!\n\n`)
+            "2 Clubs": 2,
+            "3 Clubs": 3,
+            "4 Clubs": 4,
+            "5 Clubs": 5,
+            "6 Clubs": 6,
+            "7 Clubs": 7,
+            "8 Clubs": 8,
+            "9 Clubs": 9,
+            "10 Clubs": 10,
+            "J Clubs": 11,
+            "Q Clubs": 12,
+            "K Clubs": 13,
+            "A Clubs": 14,
 
-
-// MAIN FUNCTION
-function guesser() {
-
-    // CREATES A RANDOM NUMBER BETWEEN 1 AND 10, BELOW !!!!!!
-    // let number = Math.floor(Math.random() * 10);
-
-    // JUST USE A STATIC NUMBER FOR TESTING, BELOW
-    let number = 7;
-        do {
-            // GUESS THE NUMBER
-            guess = prompt("Guess a number btwn 1-10: ");
-
-            // OUT OF MONEY 
-            if (currentWallet < 10) {
-                alert("\nWOMP WOMP!!! You do not have enough to bet again!");
-                alert(`Your current funds are $${currentWallet.toFixed(2)}`);
-                alert("Thanks for playing!")
-                playAgain = prompt("\nWould you like to play again? [ y / n ] ").toLowerCase();
-                if (playAgain == "y") {
-                    currentWallet = prompt(`\nPlease enter a new amount that you would like to play with today! Keep in mind that each guess costs $10. -$`);
-                }
-                else if (playAgain == "n") {
-                    alert(`\nThanks for visiting, we hope your loss is our gain!\n\nCashing out: $${currentWallet.toFixed(2)}`)
-                    break;
-                }
+            "2 Spades": 2,
+            "3 Spades": 3,
+            "4 Spades": 4,
+            "5 Spades": 5,
+            "6 Spades": 6,
+            "7 Spades": 7,
+            "8 Spades": 8,
+            "9 Spades": 9,
+            "10 Spades": 10,
+            "J Spades": 11,
+            "Q Spades": 12,
+            "K Spades": 13,
+            "A Spades": 14
             }
 
-            // NUMBER TOO HIGH
-            else if (number < guess) {
-                alert("\nUh-oh, that's too high!");
-                alert("Try again!\n");
-                currentWallet -= 10;
-                alert(`Your current wallet is at $${currentWallet.toFixed(2)}\n`);
+let deck2 = {"2 Hearts": 2,
+            "3 Hearts": 3,
+            "4 Hearts": 4,
+            "5 Hearts": 5,
+            "6 Hearts": 6,
+            "7 Hearts": 7,
+            "8 Hearts": 8,
+            "9 Hearts": 9,
+            "10 Hearts": 10,
+            "J Hearts": 11,
+            "Q Hearts": 12,
+            "K Hearts": 13,
+            "A Hearts": 14,
+
+            "2 Diamonds": 2,
+            "3 Diamonds": 3,
+            "4 Diamonds": 4,
+            "5 Diamonds": 5,
+            "6 Diamonds": 6,
+            "7 Diamonds": 7,
+            "8 Diamonds": 8,
+            "9 Diamonds": 9,
+            "10 Diamonds": 10,
+            "J Diamonds": 11,
+            "Q Diamonds": 12,
+            "K Diamonds": 13,
+            "A Diamonds": 14,
+
+            "2 Clubs": 2,
+            "3 Clubs": 3,
+            "4 Clubs": 4,
+            "5 Clubs": 5,
+            "6 Clubs": 6,
+            "7 Clubs": 7,
+            "8 Clubs": 8,
+            "9 Clubs": 9,
+            "10 Clubs": 10,
+            "J Clubs": 11,
+            "Q Clubs": 12,
+            "K Clubs": 13,
+            "A Clubs": 14,
+
+            "2 Spades": 2,
+            "3 Spades": 3,
+            "4 Spades": 4,
+            "5 Spades": 5,
+            "6 Spades": 6,
+            "7 Spades": 7,
+            "8 Spades": 8,
+            "9 Spades": 9,
+            "10 Spades": 10,
+            "J Spades": 11,
+            "Q Spades": 12,
+            "K Spades": 13,
+            "A Spades": 14
             }
 
-            // NUMBER TOO LOW 
-            else if (number > guess) {
-                alert("\nOuch, that's too low!");
-                alert("Try again!\n");
-                currentWallet -= 10;
-                alert(`Your current wallet is at $${currentWallet.toFixed(2)}\n`);
+
+// DECK 1 Random CARD NAME
+let player1cardPick = Object.entries(deck1)[Math.floor(Math.random()*Object.keys(deck1).length)];
+let player1cardName = player1cardPick[Object.keys(player1cardPick)[0]];
+
+// DECK 1 Random CARD VALUE
+let player1cardValue = player1cardPick[Object.keys(player1cardPick)[1]];
+// alert(player1cardValue)
+
+// DECK 2 Random CARD NAME
+let player2cardPick = Object.entries(deck2)[Math.floor(Math.random()*Object.keys(deck2).length)];
+let player2cardName = player2cardPick[Object.keys(player2cardPick)[0]];
+// alert(player2cardName)
+
+// DECK 2 Random CARD VALUE
+let player2cardValue = player2cardPick[Object.keys(player2cardPick)[1]];
+// alert(player2cardValue)
+
+let wallet = 0
+
+function warGame() {
+
+    function greeting(){
+        let greet = prompt("This is a game of War! Are you prepared for battle!?  (yes or no)")
+        if (greet == "yes") {
+            alert("Let the battle Begin!");
+        } else {
+            alert("You Are not strong enough!! Goodbye!");
+        }
+    }
+    greeting();
+
+    function startGame(){
+        let start = prompt("Your starting balance will be $100 Dollars!\n Each bet will cost you $5 dollars.\n Is this understood? (yes or no)")
+        if (start === 'yes') {
+            alert("Release the Cards!")
+        } else {
+            alert("Goodbye!");
+        }
+    }    
+
+    function play() {
+        function deal() {
+            alert(`Player 1 draws: ${player1cardName}`)
+            alert(`Player 2 draws: ${player2cardName}`)
+            p1draw = player1cardValue
+            p2draw = player2cardValue
+        }
+        deal();
+
+        function score() {
+
+            if (p1draw < p2draw) {
+                alert("p1 loses");
+                wallet -= 5
+                alert(`Your current winnings are $${wallet.toFixed(2)}`);
+            
             }
-
-            // WINNER
-            else if (number == guess) {
-                alert("\nWinner, Winner, Chicken Dinner! You guessed it!\n");
-                currentWallet += 10;
-                
-                //THIS DUMB LINE BELOW BREAKS AT RANDOM IN REPL.IT SO IF I DUPLICATE IT AND COMMENT IT OUT, AND RUN WITH THE DUPLICATE IT WORKS, WHYYY???
-                //alert(`Your winnings are $${currentWallet.toFixed(2)}. Woohoo!\n`);
-                alert(`Your winnings are $${currentWallet.toFixed(2)}. Woohoo!\n`);
-
-                // PLAY AGAIN
-                playAgain = prompt("\nWould you like to play again? [ y / n ] ").toLowerCase();
-                if (playAgain == "y") {
-                    alert(`Your current wallet is at $${currentWallet.toFixed(2)}`);
-
-                    // ADD MORE $ OR STAY WITH BALANCE
-                    newWallet = prompt(`\nAdd some more? Or enter '0' to keep current wallet... -$`);
-                    currentWallet = parseInt(currentWallet) + parseInt(newWallet);
-                    alert(`New wallet is: $${currentWallet.toFixed(2)}\n\n`);
-                    guesser();
-                }
-
-                // DONT PLAY AGAIN + CASH OUT
-                else if (playAgain == "n") {
-                    alert(`\nThanks for visiting, we hope your loss is our gain!\n\nCashing out: $${currentWallet.toFixed(2)}`);
-                    break;
-                }                
+            else if (p1draw > p2draw) {
+                alert("p1 wins")
+                wallet += 5
+                alert(`Your current winnings are $${wallet.toFixed(2)}`)
             }
-        } while (guess != number);
-        
+            else if (p1draw == p2draw) {
+                alert("its a tie")
+                wallet += 0
+                alert(`Your current winnings are $${wallet.toFixed(2)}`)
+            }
+            
+
+        }
+        score();
+
+    }
+    play();
+    function nextRound() {
+        next = prompt("\nWould you like to play again? (yes or no) ")
+                    if (next == "yes") {
+                        alert("Another $100 dollars have been placed in your wallet!");
+                        // wallet += 100
+                        alert(`Your current wallet is: $${wallet.toFixed(2)}\n`);
+                        alert('\nNext Round!\n')
+                        }
+                        play();
+                        
+    }
+    nextRound();
 }
-guesser();
+
+warGame();
--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,110 +1,192 @@
 
+let deck1 = {
+"2 Hearts": 2,
+"3 Hearts": 3,
+"4 Hearts": 4,
+"5 Hearts": 5,
+"6 Hearts": 6,
+"7 Hearts": 7,
+"8 Hearts": 8,
+"9 Hearts": 9,
+"10 Hearts": 10,
+"J Hearts": 11,
+"Q Hearts": 12,
+"K Hearts": 13,
+"A Hearts": 14,
 
+"2 Diamonds": 2,
+"3 Diamonds": 3,
+"4 Diamonds": 4,
+"5 Diamonds": 5,
+"6 Diamonds": 6,
+"7 Diamonds": 7,
+"8 Diamonds": 8,
+"9 Diamonds": 9,
+"10 Diamonds": 10,
+"J Diamonds": 11,
+"Q Diamonds": 12,
+"K Diamonds": 13,
+"A Diamonds": 14,
 
-const deck = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A',
-  2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A',
-  2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A',
-  2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A']
+"2 Clubs": 2,
+"3 Clubs": 3,
+"4 Clubs": 4,
+"5 Clubs": 5,
+"6 Clubs": 6,
+"7 Clubs": 7,
+"8 Clubs": 8,
+"9 Clubs": 9,
+"10 Clubs": 10,
+"J Clubs": 11,
+"Q Clubs": 12,
+"K Clubs": 13,
+"A Clubs": 14,
 
-class Player{
-    constructor(name, money){
-      this.name = name
-      this.wallet = money
-    }
-  
-  } 
-  const player = new Player("Challenger", 100);
-  console.log(`${player.name} => ${player.wallet}`)
-
-  
-function greeting(){
-    const greet = prompt("This is a game of War! Are you prepared for battle!?")
-    if (greet == 'yes') {
-        return ("Let the battle Begin!");
-    } else (greet == 'no')
-        return alert("You Are not strong enough!! Goodbye!");
-        
+"2 Spades": 2,
+"3 Spades": 3,
+"4 Spades": 4,
+"5 Spades": 5,
+"6 Spades": 6,
+"7 Spades": 7,
+"8 Spades": 8,
+"9 Spades": 9,
+"10 Spades": 10,
+"J Spades": 11,
+"Q Spades": 12,
+"K Spades": 13,
+"A Spades": 14
 }
-console.log(greeting())
 
-function startGame(){
-    let start = prompt("Your starting balance will be $100 Dollars!\n Each bet will cost you $5 dollars.\n Is this understood?")
-    if (start == 'yes') {
-        return ("Release the cards!!")
-    } else ( start == 'no')
-        return alert("Goodbye!");
-}    
+let deck2 = {
+"2 Hearts": 2,
+"3 Hearts": 3,
+"4 Hearts": 4,
+"5 Hearts": 5,
+"6 Hearts": 6,
+"7 Hearts": 7,
+"8 Hearts": 8,
+"9 Hearts": 9,
+"10 Hearts": 10,
+"J Hearts": 11,
+"Q Hearts": 12,
+"K Hearts": 13,
+"A Hearts": 14,
 
-console.log(startGame())
+"2 Diamonds": 2,
+"3 Diamonds": 3,
+"4 Diamonds": 4,
+"5 Diamonds": 5,
+"6 Diamonds": 6,
+"7 Diamonds": 7,
+"8 Diamonds": 8,
+"9 Diamonds": 9,
+"10 Diamonds": 10,
+"J Diamonds": 11,
+"Q Diamonds": 12,
+"K Diamonds": 13,
+"A Diamonds": 14,
 
+"2 Clubs": 2,
+"3 Clubs": 3,
+"4 Clubs": 4,
+"5 Clubs": 5,
+"6 Clubs": 6,
+"7 Clubs": 7,
+"8 Clubs": 8,
+"9 Clubs": 9,
+"10 Clubs": 10,
+"J Clubs": 11,
+"Q Clubs": 12,
+"K Clubs": 13,
+"A Clubs": 14,
 
+"2 Spades": 2,
+"3 Spades": 3,
+"4 Spades": 4,
+"5 Spades": 5,
+"6 Spades": 6,
+"7 Spades": 7,
+"8 Spades": 8,
+"9 Spades": 9,
+"10 Spades": 10,
+"J Spades": 11,
+"Q Spades": 12,
+"K Spades": 13,
+"A Spades": 14
+}
 
-  
-  const valuePoints = {
-    2 : 2,
-    3 : 3,
-    4 : 4,
-    5 : 5,
-    6 : 6,
-    7 : 7,
-    8 : 8,
-    9 : 9,
-    10 : 10,
-    "J" : 10,
-    "Q" : 10,
-    "K" : 10,
-    "A" : 11
-  }
-  
-  
-  
-  const deckShapes = ['Hearts', 'Diamonds', 'Spades', 'Clubs']
-  
-  const randomNum = deck[Math.floor(Math.random()* deck.length)]
-  const randomShape = deckShapes[Math.floor(Math.random()* deckShapes.length)]
-  
-  console.log(randomNum, randomShape)
-  
-  
-  const deck2 = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A',
-  2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A',
-  2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A',
-  2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A']
-  
-  const deckShapes2 = ['Hearts', 'Diamonds', 'Spades', 'Clubs']
-  
-  const randomNum2 = deck2[Math.floor(Math.random()* deck2.length)]
-  const randomShape2 = deckShapes2[Math.floor(Math.random()* deckShapes2.length)]
-  
-  console.log(randomNum2, randomShape2)
+let player1cardPick = Object.entries(deck1)[Math.floor(Math.random()*Object.keys(deck1).length)];
+let player1cardName = player1cardPick[Object.keys(player1cardPick)[0]];
 
-function deal(){
-    
-    if (player.wallet < 5) {
-        alert("You do not have enough to bet again!");
-        alert(`Your current funds are $${player.wallet.toFixed(2)}`);
-        alert("You have lost the battle!")
-        playAgain = prompt("\nWould you like to play again? [ y / n ] ")
-        if (playAgain == "y") {
-            player.wallet = prompt("Another $100 dollars have been placed in your wallet!");
-        } else (playAgain == "n");
-            alert(`Thanks for playing! Cash out amout is - $${player.wallet.toFixed(2)}`)
-        
-    }
-   
-} 
+let player1cardValue = player1cardPick[Object.keys(player1cardPick)[1]];
+
+let player2cardPick = Object.entries(deck2)[Math.floor(Math.random()*Object.keys(deck2).length)];
+let player2cardName = player2cardPick[Object.keys(player2cardPick)[0]];
+
+let player2cardValue = player2cardPick[Object.keys(player2cardPick)[1]];
+
+let wallet = 100;
+let computer;
+let challenger;
+
+function warGame() {
+    function greeting(){
+        const greet = prompt("This is a game of War! Are you prepared for battle!?")
+        if (greet === 'yes') {
+            return alert("Let the battle Begin!");
+        } else (greet === 'no')
+            return alert("You Are not strong enough!! Goodbye!");
             
+    }
+    greeting()
 
-    if (deck > deck2) {
-        alert("The House Wins!");
-        player.wallet -= 5;
-        alert(`Your current winnings are $${player.wallet.toFixed(2)}`);
-    } else if (deck < deck2) {
-         alert("You Win!!");
-         alert(`Your current winnings are $${player.wallet.toFixed(2)}`);
-        player.wallet += 5;
-        } else (deck == deck2);
-            alert("Its a Tie!! Deal again");
-            alert(`Your current winnings are $${player.wallet.toFixed(2)}`);
-    
+    function startGame(){
+        let start = prompt("Your starting balance will be $100 Dollars!\n Each bet will cost you $5 dollars.\n Is this understood?")
+        if (start === 'yes') {
+            return alert("Release the cards!!")
+        } else (start === 'no')
+            return alert("Goodbye!");
+    }    
+
+    console.log(startGame())
+
+    function play() {
+        function deal() {
+            alert(`Computer draws: ${player1cardName}`)
+            alert(`Challenger draws: ${player2cardName}`)
+            computer = player1cardValue;
+            challenger = player2cardValue;
+        
+
+            if (computer < challenger) {
+                alert("Challenger Wins!");
+                wallet += 5;
+                return alert(`Your current winnings are $${wallet.toFixed(2)}`);
+            
+            } else if (computer > challenger) {
+                alert("Challenger Loses!");
+                wallet -= 5;
+                return alert(`Your current winnings are $${wallet.toFixed(2)}`);
+            } else if (computer == challenger)
+                return alert("Its a tie");
+                wallet += 0;
+                return alert(`Your current winnings are $${wallet.toFixed(2)}`);
+        }
+        deal();
+  
+
+        const next = prompt("Would you like to play again?")
+            if (next === "yes") {
+                return deal();
+                //alert("Another $100 dollars have been placed in your wallet!");
+                // wallet += 100
+                //alert(`Your current wallet is: $${wallet.toFixed(2)}\n`);
+                //alert('\nNext Round!\n')
+            } else if(next === "no")
+                return alert(`Your final balance is: $${wallet.toFixed(2)}\n`);
+        }
+    play();
+}
+warGame();
+
 
